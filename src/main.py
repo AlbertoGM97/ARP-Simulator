fa2b214c4b34253fd9be0b655280b4c367396b0f
Alberto García Merino <alberto.garciamerino97@gmail.com>
2017-05-06 19:13:55 +0000

    Entrada de IP pasada a main

--- src/main.py
+++ src/main.py
@@ -2,17 +2,16 @@
 
 from readInput import readInput
 
-def main(inputScenario):
+def main(topologia):
+    IPorigin = input("Enter the IP of the source for the packet: ")
+    IPdest   = input("Enter the IP of the destination for the packet: ")
 
-    for a in inputScenario["subnets"]:
+    for a in topologia["subnets"]: # Esto simplemente es de ejemplo
         print(a["id"])
 
-if __name__ == "__main__":
+
+
+if __name__ == "__main__":    # El código va en main, no aqui
     
     inString = readInput("infile.json")
     main(inString)
-    
-IPorigin=input("Enter the IP of the source for the packet")
-IPdest=input("Enter the IP of the destination for the packet")
-
-
