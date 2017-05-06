5dea7ba3cbbf82096895c389f3befa0a6f2730a7
Álvaro Molinero Fernández <alvaromolinerofernandez@gmail.com>
2017-05-06 17:12:25 +0000

    main.py edited online with Bitbucket

--- src/main.py
+++ src/main.py
@@ -11,3 +11,8 @@ if __name__ == "__main__":
     
     inString = readInput("infile.json")
     main(inString)
+    
+IPorigin=input("Enter the IP of the source for the packet")
+IPdest=input("Enter the IP of the destination for the packet")
+
+
