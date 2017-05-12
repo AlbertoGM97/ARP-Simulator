f8fd39b4711b9ef5a0362256aad4da1744d94e0d
Luis Rodriguez <luis.frc24@gmail.com>
2017-05-12 14:06:31 +0000

    layer3.py edited online with Bitbucket save_ARP_table Findinterface send_frame receive_frame is_one_of_your_neighbors

--- src/layer3.py
+++ src/layer3.py
@@ -30,10 +30,17 @@ class layer3_device:
         pass
         
     def findinterface(IP_next):
-        pass
+        for i in range(0,len(self.ifaces)): # Search among its ifaces objects if one has that IP
+            if self.ifaces[i].is_one_of_your_neighbors(IP_next):
+                return self.ifaces[i];
 
     def save_ARP_table(self, iface):
-        pass
+        count=0
+        for i in range(0,len(self.ARP_table)): # Check if IP to save is in ARP table
+            if self.ARP_table[i].IP==iface.IP_addr && self.ARP_table[i].MAC==iface.MAC_addr
+                count++
+        if count==0:
+            ARP_table[len(ARP_table)] =  iface.MAC_addr, iface.IP_addr # Save it in ARP table--no estoy seguro de como se hace
 
 class iface:
 
@@ -54,10 +61,22 @@ class iface:
         pass
 
     def send_frame(self, MAC, IP_dest):
-        pass
+        print IP_addr+"sends to"+"IP_dest with MAC"+MAC
+        for i in range(0,len(self.adjacent)): # Search among its adjacent objects if one has that IP
+            if self.adjacent[i].MAC_addr==MAC:
+                self.adjacent[i].receive_frame(IP_dest);
 
     def receive_frame(self, IP_dest):
-        pass
+        if IP_dest==IP_addr:
+            print"Packet arrived to destination."
+        else
+            self.layer3_parent.send_packet(IP_dest)
+        
+    def is_one_of_your_neighbors(IP_search):
+        for i in range(0,len(self.adjacent)): # Search among its ifaces objects if one has that IP
+            if self.adjacent[i].IP_addr==IP_search:
+                return true;
+        return false;
 
 class IP_utils:
 
