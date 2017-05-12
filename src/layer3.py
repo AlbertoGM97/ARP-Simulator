edd1c3f0df3810f576c97ff96e6bd7637d36e0e1
Alberto García Merino <alberto.garciamerino97@gmail.com>
2017-05-12 14:16:25 +0000

    Fixed ":", PY3 compatibility and x++ stuff (again)

--- src/layer3.py
+++ src/layer3.py
@@ -8,21 +8,21 @@ class layer3_device:
         self.ARP_table = []         # Empty list for ARP table.     REVIEW: Is it useless?  R2: Why do you say so?
 
     def is_your_IP(self, IP_search):
-        for i in range(0,len(self.ifaces)): # Search among its ifaces objects if one has that IP
-            if self.ifaces[i].IP_addr==IP_search:
+        for i in range(0, len(self.ifaces)):    # Search among its ifaces objects if one has that IP
+            if self.ifaces[i].IP_addr == IP_search:
                 return true;
         return false;
 
     def send_packet(self, IP_dest):
-        print "Sending packet to"+IP_dest+"." 
-        IP_next=findIPnext(IP_dest)    # Search in routing table for IP to send
-        interface = findinterface(IP_next) # Search which our own interfaces sends to that IP
-        count=0
-        for i in range(0,len(self.ARP_table)): # Check if IP to send is in ARP table
-            if self.ARP_table[i].IP==IP_next:
-                count++
-        if count==0:
-            print "is not in ARP table. Sending ARP broadcast"
+        print("Sending packet to" + IP_dest + ".")
+        IP_next = findIPnext(IP_dest)            # Search in routing table for IP to send
+        interface = findinterface(IP_next)       # Search which our own interfaces sends to that IP
+        count = 0
+        for i in range(0, len(self.ARP_table)):  # Check if IP to send is in ARP table
+            if self.ARP_table[i].IP == IP_next:
+                count = count+1
+        if count == 0:
+            print("is not in ARP table. Sending ARP broadcast")
             ARP_table[len(ARP_table)] = interface.send_ARP(IP_next) # Ask for the MAC of IP to send--no estoy seguro este bien puesto
         interface.send_frame(MAC, IP_dest)
         
@@ -30,17 +30,17 @@ class layer3_device:
         pass
         
     def findinterface(IP_next):
-        for i in range(0,len(self.ifaces)): # Search among its ifaces objects if one has that IP
+        for i in range(0, len(self.ifaces)):          # Search among its ifaces objects if one has that IP
             if self.ifaces[i].is_one_of_your_neighbors(IP_next):
                 return self.ifaces[i];
 
     def save_ARP_table(self, iface):
-        count=0
-        for i in range(0,len(self.ARP_table)): # Check if IP to save is in ARP table
-            if self.ARP_table[i].IP==iface.IP_addr && self.ARP_table[i].MAC==iface.MAC_addr
-                count++
-        if count==0:
-            ARP_table[len(ARP_table)] =  iface.MAC_addr, iface.IP_addr # Save it in ARP table--no estoy seguro de como se hace
+        count = 0
+        for i in range(0,len(self.ARP_table)):        # Check if IP to save is in ARP table
+            if (self.ARP_table[i].IP == iface.IP_addr) && (self.ARP_table[i].MAC==iface.MAC_addr):
+                count = count+1
+        if count == 0:
+            ARP_table[len(ARP_table)] =  iface.MAC_addr, iface.IP_addr    # Save it in ARP table--no estoy seguro de como se hace
 
 class iface:
 
@@ -61,22 +61,22 @@ class iface:
         pass
 
     def send_frame(self, MAC, IP_dest):
-        print IP_addr+"sends to"+"IP_dest with MAC"+MAC
-        for i in range(0,len(self.adjacent)): # Search among its adjacent objects if one has that IP
-            if self.adjacent[i].MAC_addr==MAC:
+        print(IP_addr + "sends to" + "IP_dest with MAC" + MAC)
+        for i in range(0, len(self.adjacent)): # Search among its adjacent objects if one has that IP
+            if self.adjacent[i].MAC_addr == MAC:
                 self.adjacent[i].receive_frame(IP_dest);
 
     def receive_frame(self, IP_dest):
-        if IP_dest==IP_addr:
-            print"Packet arrived to destination."
-        else
+        if IP_dest == IP_addr:
+            print("Packet arrived to destination.")
+        else:
             self.layer3_parent.send_packet(IP_dest)
         
     def is_one_of_your_neighbors(IP_search):
         for i in range(0,len(self.adjacent)): # Search among its ifaces objects if one has that IP
-            if self.adjacent[i].IP_addr==IP_search:
-                return true;
-        return false;
+            if self.adjacent[i].IP_addr == IP_search:
+                return true
+        return false
 
 class IP_utils:
 
